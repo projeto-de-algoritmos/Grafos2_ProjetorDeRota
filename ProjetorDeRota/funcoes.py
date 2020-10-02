8bb6a7b9b30f598ef6eb24d800385705ed8c3333
MoacirMSJ <moacirmascarenha86@gmail.com>
2020-10-02 00:13:33 -0300

    Adicionado dijkstra ao arquivo de funções.

--- ProjetorDeRota/funcoes.py
+++ ProjetorDeRota/funcoes.py
@@ -1,3 +1,7 @@
+from queue import PriorityQueue
+from math import inf
+
+
 def criar_mapa():
     qtd_cidade = 0
     while(qtd_cidade <= 0):
@@ -15,5 +19,33 @@ def criar_mapa():
             nome_vizinho, distancia = input("Nome da cidade(vizinha) e a distancia: ").split()
             mapa.append((nome_cidade, nome_vizinho, distancia))
 
-    
-    
+    return mapa
+
+def dijkstra(graph, root):
+    queue = PriorityQueue()  
+    path = {}  
+    for v in graph.vertices():
+        if v == root:
+            path[v] = [[], 0]  
+        else:
+            path[v] = [[], inf]  
+
+        queue.put((path[v][1], v))  
+
+    remaing_vertices = list(graph.vertices()) 
+
+    for i in range(len(graph.vertices())):
+        u = queue.get()[1]  
+        remaing_vertices.remove(u) 
+
+        for v in remaing_vertices:  
+            du = path[u][1] 
+            w = graph.direct_cost(u, v) 
+            dv = path[v][1]  
+            if du + w < dv:  
+                path[v][1] = du + w  
+                path[v][0] = path[u][0] + [u]  
+                queue.queue.remove((dv, v)) 
+                queue.put((path[v][1], v))
+
+    return path
