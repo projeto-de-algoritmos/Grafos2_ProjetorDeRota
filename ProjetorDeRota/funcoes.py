f4e25458eb81d39c7f5d8a6ffc2cfd310d0bc009
MoacirMSJ <moacirmascarenha86@gmail.com>
2020-10-01 21:41:55 -0300

    Criada funcao criar_mapa

---
+++ ProjetorDeRota/funcoes.py
@@ -0,0 +1,19 @@
+def criar_mapa():
+    qtd_cidade = 0
+    while(qtd_cidade <= 0):
+        qtd_cidade = int(input("Quantidade de cidades: " ))
+    
+    mapa = []
+
+    for i in range(qtd_cidade):
+        nome_cidade = input("Nome da cidade: " )
+        qtd_vizinhos = 0
+        while(qtd_vizinhos <= 0):
+            qtd_vizinhos = int(input("Numero de cidades vizinhas: " ))
+
+        for j in range(qtd_vizinhos):
+            nome_vizinho, distancia = input("Nome da cidade(vizinha) e a distancia: ").split()
+            mapa.append((nome_cidade, nome_vizinho, distancia))
+
+    
+    
